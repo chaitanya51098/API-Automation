d5cc4e5dd290be9e413b5c9b8229a607f5748d84
chaitanya51098 <skc51098@gmail.com>
2022-01-03 17:58:36 +0530

    api automation

---
+++ src/test/java/example.java
@@ -0,0 +1,29 @@
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.*;
+
+public class example {
+
+    @Test
+    public void func_1(){
+        Response response=get("https://reqres.in/api/users/2");
+        //response.getStatusCode();
+        //System.out.println(response.getStatusCode());
+        Assert.assertEquals(response.getStatusCode(),200);
+        System.out.println(response.getBody().asString());
+        System.out.println(response.getStatusLine());
+        System.out.println(response.getTime());
+    }
+    @Test
+    public void func_2(){
+    baseURI="https://reqres.in";
+    given().get("/api/users/2").
+            then().
+            statusCode(200).
+            body("data.id",equalTo(2))
+            .log().all();
+
+    }
+}
